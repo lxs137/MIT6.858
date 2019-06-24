ae050b1c5b947675192632a7cabb56a7c8d820f6
lxs <lxs137@hotmail.com>
2019-06-24 20:58:48 +0800

    lab3: ex-5

--- lab3/symex/symsql.py
+++ lab3/symex/symsql.py
@@ -7,12 +7,22 @@ import sqlalchemy.orm
 oldget = sqlalchemy.orm.query.Query.get
 def newget(query, primary_key):
   ## Exercise 5: your code here.
-  ##
+  ##z
   ## Find the object with the primary key "primary_key" in SQLalchemy
   ## query object "query", and do so in a symbolic-friendly way.
   ##
   ## Hint: given a SQLalchemy row object r, you can find the name of
   ## its primary key using r.__table__.primary_key.columns.keys()[0]
+
+  rows = sqlalchemy.orm.query.Query.all(query)
+  if len(rows) == 0:
+  	return None
+  key = rows[0].__table__.primary_key.columns.keys()[0]
+
+  for row in rows:
+  	if primary_key == vars(row)[key]:
+  		return row
+
   return None
 
 sqlalchemy.orm.query.Query.get = newget
